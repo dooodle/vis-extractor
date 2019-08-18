c166f794bee2df0aa3f97b59465dacda2b7a5879
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-08-18 10:52:39 +0100

    add more serialisation logic

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/knakk/rdf"
+	_ "github.com/lib/pq"
 	"io"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-	_ "github.com/lib/pq"
-	"github.com/knakk/rdf"
 )
+
 // https://newfivefour.com/postgresql-information-schema.html
 
 var user = os.Getenv("VIS_MONDIAL_USER")
@@ -22,6 +21,16 @@ var sslmode = os.Getenv("VIS_MONDIAL_SSLMODE")
 
 var db *sql.DB
 
+const (
+	rootPrefix        = "http://dooodle/"
+	colPrefix         = rootPrefix + "column/"
+	tablePrefix       = rootPrefix + "entity/"
+	predPrefix        = rootPrefix + "predicate/"
+	dataTypePrefix    = rootPrefix + "dataType/"
+	discreteDimension = rootPrefix + "dimension/discrete"
+	scalarDimension   = rootPrefix + "dimension/scalar"
+)
+
 func init() {
 	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=%s", user, dbname, password, host, port, sslmode)
 	var err error
@@ -31,12 +40,13 @@ func init() {
 	}
 }
 
-
 func main() {
 	fmt.Printf("starting db graph extractor for %s on %s:%s\n", dbname, host, port)
 	//q := "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
 	//WriteQuery(os.Stdout,q,false,false)
-	writeTableColS(os.Stdout,100)
+	writeTableColS(os.Stdout)
+	writeColsDataType(os.Stdout)
+	writeScalarOrDiscrete(os.Stdout, 100)
 }
 
 //– discrete dimensions have a relatively small number of distinct values, that
@@ -49,55 +59,7 @@ func main() {
 //when extracting use limit to decide if its scalar or discrete
 func writeScalarOrDiscrete(w io.Writer, limit int) {
 	//q := "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
-
-	q := `SELECT columns.table_name,
-		  columns.column_name,
-		  columns.data_type,
-		  columns.is_nullable
-	FROM information_schema.columns
-	LEFT JOIN information_schema.tables ON columns.table_name = tables.table_name
-	WHERE tables.table_schema = 'public' 
-	`
-
-	rows, _ := db.Query(q)
-
-	data := struct{
-		tableName string
-		colName string
-		dataType string
-		isNullable string
-	}{}
-
-	defer rows.Close()
-
-	triples := []rdf.Triple{}
-
-	for rows.Next() {
-		err := rows.Scan(&(data.tableName),&(data.colName),&(data.dataType),&(data.isNullable))
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		subject, _ := rdf.NewIRI("http://dooodle/" + data.tableName)
-		pred,_ := rdf.NewIRI("http://dooodle/hasColumn")
-		object, _ := rdf.NewIRI("http://dooodle/" + data.colName)
-		triples = append(triples,rdf.Triple{
-			Subj : subject,
-			Pred: pred,
-			Obj: object,
-		})
-		//fmt.Println(data)
-	}
-	for _, t := range triples {
-		str := t.Serialize(rdf.NTriples)
-		w.Write([]byte(str))
-	}
-
-}
-
-func writeTableColS(w io.Writer, limit int) {
-	//q := "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
-fmt.Println("entering")
+	fmt.Println("entering")
 	q := `SELECT columns.table_name,
 		  columns.column_name
 	FROM information_schema.columns
@@ -107,35 +69,107 @@ fmt.Println("entering")
 
 	rows, _ := db.Query(q)
 
-	data := struct{
+	data := struct {
 		tableName string
-		colName string
+		colName   string
 	}{}
 
 	defer rows.Close()
 	triples := []rdf.Triple{}
 
 	for rows.Next() {
-		err := rows.Scan(&(data.tableName),&(data.colName))
+		err := rows.Scan(&(data.tableName), &(data.colName))
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		subject, _ := rdf.NewIRI("http://dooodle/" + data.tableName)
-		pred,_ := rdf.NewIRI("http://dooodle/hasColumn")
-		object, _ := rdf.NewIRI("http://dooodle/" + data.colName)
-		triple := rdf.Triple{
-			Subj : subject,
-			Pred: pred,
-			Obj: object,
+		subQuery := fmt.Sprintf("SELECT COUNT (DISTINCT %s) FROM %s", data.colName, data.tableName)
+		subRows, err := db.Query(subQuery)
+		if err != nil {
+			fmt.Println(err)
 		}
-		triples = append(triples,triple)
+
+		for subRows.Next() {
+			count := 0
+			err := subRows.Scan(&count)
+			if err != nil {
+				fmt.Println(err)
+			}
+			subject, _ := rdf.NewIRI(tablePrefix + data.colName)
+			pred, _ := rdf.NewIRI(predPrefix + "numDistinct")
+			object, _ := rdf.NewLiteral(count)
+			triple := rdf.Triple{
+				Subj: subject,
+				Pred: pred,
+				Obj:  object,
+			}
+			triples = append(triples, triple)
+
+			dSubject, _ := rdf.NewIRI(tablePrefix + data.colName)
+			dPred, _ := rdf.NewIRI(predPrefix + "hasDimension")
+			var dObject rdf.IRI
+			switch {
+			case count <= 100:
+				dObject, err = rdf.NewIRI(discreteDimension)
+				if err != nil {
+					fmt.Println(err)
+				}
+			default:
+				dObject, err = rdf.NewIRI(scalarDimension)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+			dtriple := rdf.Triple{
+				Subj: dSubject,
+				Pred: dPred,
+				Obj:  dObject,
+			}
+			triples = append(triples, dtriple)
+		}
 	}
 
-	//for _, t := range triples {
-	//	str := t.Serialize(rdf.NTriples)
-	//	w.Write([]byte(str))
-	//}
+	for _, t := range triples {
+		str := t.Serialize(rdf.Turtle)
+		w.Write([]byte(str))
+	}
+
+}
+
+func writeTableColS(w io.Writer) {
+	q := `SELECT columns.table_name,
+		  columns.column_name
+	FROM information_schema.columns
+	LEFT JOIN information_schema.tables ON columns.table_name = tables.table_name
+	WHERE tables.table_schema = 'public' 
+	`
+
+	rows, _ := db.Query(q)
+
+	data := struct {
+		tableName string
+		colName   string
+	}{}
+
+	defer rows.Close()
+	triples := []rdf.Triple{}
+
+	for rows.Next() {
+		err := rows.Scan(&(data.tableName), &(data.colName))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		subject, _ := rdf.NewIRI(tablePrefix + data.tableName)
+		pred, _ := rdf.NewIRI(predPrefix + "hasColumn")
+		object, _ := rdf.NewIRI(colPrefix + data.colName)
+		triple := rdf.Triple{
+			Subj: subject,
+			Pred: pred,
+			Obj:  object,
+		}
+		triples = append(triples, triple)
+	}
 
 	for _, t := range triples {
 		str := t.Serialize(rdf.Turtle)
@@ -143,61 +177,46 @@ fmt.Println("entering")
 	}
 }
 
-func WriteQuery(w io.Writer, q string, header bool, null bool) {
-	connStr := fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=%s", user, dbname, password, host, port, sslmode)
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		w.Write([]byte(err.Error()))
-		log.Fatal(err)
-	}
-	defer db.Close()
+func writeColsDataType(w io.Writer) {
 
-	rows, err := db.Query(q)
-	if err != nil {
-		w.Write([]byte(err.Error()))
-		return
-	}
+	q := `SELECT columns.column_name,
+		  columns.data_type,
+		  columns.udt_name
+	FROM information_schema.columns
+	LEFT JOIN information_schema.tables ON columns.table_name = tables.table_name
+	WHERE tables.table_schema = 'public' 
+	`
+
+	rows, _ := db.Query(q)
+
+	data := struct {
+		colName  string
+		dataType string
+		udtName  string
+	}{}
+
 	defer rows.Close()
-	cols, err := rows.Columns()
-	if header {
-		w.Write([]byte(strings.Join(cols, ",") + "\n"))
-	}
-	if err != nil {
-		log.Fatalf("Columns: %v", err)
-	}
+	triples := []rdf.Triple{}
 
-	_vals := make([]sql.NullString, len(cols))
-	vals := make([]interface{}, len(cols))
-	for i, _ := range cols {
-		vals[i] = &_vals[i]
-	}
-
-	hasOneInvalidCol := false
 	for rows.Next() {
-		err := rows.Scan(vals...)
+		err := rows.Scan(&(data.colName), &(data.dataType), &(data.udtName))
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
 		}
-		outs := make([]string, 0)
-		for i := range cols {
-			v := vals[i].(*sql.NullString)
-			if v.Valid {
-				outs = append(outs, strconv.Quote(v.String))
-			} else {
-				hasOneInvalidCol = true
-				outs = append(outs, "")
-			}
+
+		subject, _ := rdf.NewIRI(colPrefix + data.colName)
+		pred, _ := rdf.NewIRI(predPrefix + "hasDataType")
+		object, _ := rdf.NewIRI(dataTypePrefix + data.udtName)
+		triple := rdf.Triple{
+			Subj: subject,
+			Pred: pred,
+			Obj:  object,
 		}
-		if !hasOneInvalidCol {
-			csv := strings.Join(outs, ",")
-			w.Write([]byte(csv + "\n"))
-		}
-		//reset flag
-		hasOneInvalidCol = false
+		triples = append(triples, triple)
 	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
+
+	for _, t := range triples {
+		str := t.Serialize(rdf.Turtle)
+		w.Write([]byte(str))
 	}
 }
-
